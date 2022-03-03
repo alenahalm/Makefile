2a44acf1a8fce3073d409f720f98b1b2be55196d
alenahalm <75882124+alenahalm@users.noreply.github.com>
2022-03-03 17:26:53 +0800

    Update second.cpp

--- second.cpp
+++ second.cpp
@@ -3,15 +3,15 @@
 #include <fstream>
 #include <cmath>
 
-using namespace std;
-
 int main() {
-    ifstream file("out.txt");
-    for(int i = 0; i < 12; i++) {
+    std::ifstream file("out.txt");
+    int n;
+    file >> n;
+    for(int i = 0; i < n; i++) {
         double x, y, z;
         file >> x >> y >> z;
         double angle = 2 * sin(x) * cos(y) + cos(z);
-        cout << angle * (180/M_PI) << endl;
+        std::cout << angle * (180/M_PI) << std::endl;
     }
-
+    file.close();
 }
