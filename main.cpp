367931830966d94e146405b65109c87289dd805a
alenahalm <75882124+alenahalm@users.noreply.github.com>
2022-03-03 17:26:39 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,16 +8,22 @@ double rand_double(double min, double max)
     return min + ((double)rand() / RAND_MAX) * (max - min);
 }
 
+int get_NUM(int argc, char *argv[]) {
+    int n;
+    for (int i = 1; i < argc; ++i) {
+        n = atoi(argv[i]);
+    }
+    return n;
+}
 
 int main(int argc, char *argv[])
 {
-    int n = 12;
-    std::ofstream f("out.txt");
+    int n = atoi(argv[1]);
+    std::cout << n << std::endl;
     for (int i = 0; i < n; ++i)
     {
-        f << rand_double(-M_PI, M_PI) << " "
+        std::cout << rand_double(-M_PI, M_PI) << " "
         << rand_double(-M_PI, M_PI) << " "
         << rand_double(-M_PI, M_PI) << std::endl;
     }
-    f.close();
 }
